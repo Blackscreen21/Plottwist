dfc8e279ff4408f1bd030195a8f6638fe91f0ded
Julian Reip <julian.reip@edu.fh-joanneum.at>
2026-01-20 17:01:02 +0100

    This will be the class that handles the API Calls of googleAPI.

---
+++ app/src/main/java/API_Handling/ApiCaller.kt
@@ -0,0 +1,15 @@
+package API_Handling
+
+import androidx.compose.material3.Text
+
+/**
+* We are using <a href = "https://www.googleapis.com/books/v1/volumes?q=isbn:">Google Rest Api as ressource</a>
+*
+**/
+class ApiCaller (
+    val isbn: String
+){
+
+}
+
+
